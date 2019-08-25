623296cb7aed0c7bdd8a4413f304123862d5e6c7
Mathuba <Mathuba58@gmail.com>
2019-08-26 01:02:15 +0200

    Added code to bwt string

--- bwt.py
+++ bwt.py
@@ -1,8 +1,21 @@
 # python3
 import sys
 
+def build_suffix_array(text):
+    t_len = len(text)
+    result = [0] * t_len
+
+    suffixes = [ (i, text[i:]) for i in range(t_len)]
+    suffixes.sort(key=lambda x: x[1])
+
+    for i, _ in enumerate(suffixes):
+        result[i] = suffixes[i][0]
+
+    return result
+
 def BWT(text):
-    return ""
+    suf_array = build_suffix_array(text)
+    return "".join(text[num_value-1] for num_value in suf_array )
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
